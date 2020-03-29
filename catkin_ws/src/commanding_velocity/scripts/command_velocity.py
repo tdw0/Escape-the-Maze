f828c5ed7451db5cfb8a005fd3d104cb42a16f45
Tom Woodward <wtom.hawk@Gmail.com>
2020-03-29 02:34:36 +0100

    Follows walls. TODO: Optimise and add tile recognition (red/green)

--- catkin_ws/src/commanding_velocity/scripts/command_velocity.py
+++ catkin_ws/src/commanding_velocity/scripts/command_velocity.py
@@ -3,50 +3,126 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 
-class GoForward():
-    def __init__(self):
-        # initiliaze
-        rospy.init_node('GoForward', anonymous=False)
 
-	# tell user how to stop TurtleBot
-	rospy.loginfo("To stop TurtleBot CTRL + C")
-
-        # What function to call when you ctrl + c    
-        rospy.on_shutdown(self.shutdown)
+class MazeSolver:
         
-	# Create a publisher which can "talk" to TurtleBot and tell it to move
-        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-     
-	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
-        r = rospy.Rate(10);
+        def __init__(self):
+                # initialise node and set the rate
+                rospy.init_node('MazeSolver')
+                self.rate = rospy.Rate(10)
+                
+                # velocity/laser/odom/image sensors
+                self.vel = Twist()
+                self.laser = None
+                self.odom = None
 
-        # Twist is a datatype for velocity
-        move_cmd = Twist()
-	# let's go forward at 0.2 m/s
-        move_cmd.linear.x = 0.2
-	# let's turn at 0 radians/s
-	move_cmd.angular.z = 0
+                # laserscan and odometer subscribers
+                rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)
+                #rospy.Subscriber('/odom', Odometry, self.odom_callback)
 
-	# as long as you haven't ctrl + c keeping doing...
-        while not rospy.is_shutdown():
-	    # publish the velocity
-            self.cmd_vel.publish(move_cmd)
-	    # wait for 0.1 seconds (10 HZ) and publish again
-            r.sleep()
-                        
+                # publisher for velocity
+                self.cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size = 5)
+                
+                # for moving the robot
+                self.mv = Twist()
+                self.mv.linear.x = 0.0
+                self.mv.angular.z = 0.0
+
+                # initialise variables for wall detection
+                self.f_range = [] # front
+                self.r_range = [] # right
+                self.l_range = [] # left
+                
+                self.f_min = 0 # front
+                self.r_min = 0 # right
+                self.l_min = 0 # left
+
+                self.near_wall = False
+                rospy.sleep(1)
+                self.solve_maze()
+
+                
+                
+        def solve_maze(self):
+                while not rospy.is_shutdown():
+                        while (self.near_wall == False and not rospy.is_shutdown()):
+                                # move to wall
+                                self.move_to_wall()
+                                print(self.near_wall)
+                        else:
+                                self.follow_wall()
+
+        def laserscan_callback(self, laser_msg):
+                self.laser = laser_msg
+                # value at in front of robot (90 degrees)
+                self.f_range = laser_msg.ranges[315:325]
+                #value to the right of robot (180 degrees)
+                self.l_range = laser_msg.ranges[629:639]
+                # value to the left of robot (0 degrees)
+                self.r_range = laser_msg.ranges[0:10]
+                
+                self.f_min = min(self.f_range)
+                self.r_min = min(self.r_range)
+                self.l_min = min(self.l_range)
+
+
+        def move_to_wall(self):
+                #print('moving towards a wall')
+                if (self.f_min > 0.5 and self.r_min > 0.5 and self.l_min > 0.5): # checking if anything is near
+                        #print(self.f_range)
+                        print(self.l_min)
+                        self.mv.angular.z = -0.1
+                        self.mv.linear.x = 0.15 # go forward
+                elif (self.l_min < 0.5):
+                        print(self.l_min)
+                        self.near_wall = True
+                        print('near left wall')
+                else:
+                        self.mv.angular.z = -0.25
+                        self.mv.linear.x = 0.0
+
+                self.cmd_vel_pub.publish(self.mv)
         
-    def shutdown(self):
-        # stop turtlebot
-        rospy.loginfo("Stop TurtleBot")
-	# a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
-        self.cmd_vel.publish(Twist())
-	# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
-        rospy.sleep(1)
- 
-if __name__ == '__main__':
-    try:
-        GoForward()
-    except:
-        rospy.loginfo("GoForward node terminated.")
+        def follow_wall(self):
+                if (self.f_min > 0.5): # start following the left wall
+                        if (self.l_min < 0.3): # if too close to the left wall reverse a bit
+                                print(self.l_min, ': Too close, Reversing')
+                                self.mv.angular.z = -1.2
+                                self.mv.linear.x = -0.1
+                        elif(self.l_min > 0.5): # follow by zig-zagging
+                                print(self.l_min)
+                                print('following wall')
+                                self.mv.angular.z = 1.2
+                                self.mv.linear.x = 0.15
+                        else:
+                                print('Turning right')
+                                self.mv.angular.z = -1.2
+                                self.mv.linear.x = 0.15
+                else:
+                        self.turn_corner()
+                
+                self.cmd_vel_pub.publish(self.mv)
+
+        def turn_corner(self):
+                print('obstacle detected, turning')
+                self.mv.angular.z = -1.0
+                self.mv.linear.x = 0.0
+                self.cmd_vel_pub.publish(self.mv)
+                while(self.l_min < 0.3 and not rospy.is_shutdown()):
+                        pass
+
+
+        def shutdown(self):
+                # stop turtlebot
+                rospy.loginfo("Stop TurtleBot")
+                # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+                self.cmd_vel.publish(Twist())
+                # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+                rospy.sleep(1)
+
+if __name__ == "__main__":
+        maze_solver = MazeSolver()
+        rospy.spin()
